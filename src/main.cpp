56a3205d47d4b1166f0118835c6f86a0527f0fb6
JulianAlejandro <julianmala2797@gmai.com>
2026-06-19 11:56:51 +0200

    cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,24 @@ byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 1, 150); 
 uint16_t modbusPort = 502;    
 
-ModbusRTUModule rtuModule(BAUDRATE);
-ModbusTCPModule tcpModule(modbusPort, &rtuModule);
+ModbusRTUModule slaveRtuModule(BAUDRATE);
+ModbusTCPModule MasterTcpModule(modbusPort, &slaveRtuModule);
 
 void setup() {
   Serial.begin(115200);
   while(!Serial){}
   Serial.println("\n--- PASARELA MODBUS TCP A RTU MODULAR ---");
 
-  if(!rtuModule.begin()){   
+  if(!slaveRtuModule.begin()){   
     Serial.println("Error RTU");    
   } else {
     Serial.println("RTU listo.");
   }
 
-  tcpModule.begin(mac, ip);
+  MasterTcpModule.begin(mac, ip);
   Serial.println("TCP listo."); 
 }
 
 void loop() {
-  tcpModule.process();
+  MasterTcpModule.process();
 }
